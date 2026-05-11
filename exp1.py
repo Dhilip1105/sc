23df8dfbb7934f88ddbbbe086412d4a788c4dbda
Dhilip C <dhilipdp51@gmail.com>
2026-05-11 06:12:08 +0530

    Create exp1.py

---
+++ exp1.py
@@ -0,0 +1,26 @@
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+
+temperature = ctrl.Antecedent(np.arange(0, 101, 1), 'temperature')
+fan_speed = ctrl.Consequent(np.arange(0, 101, 1), 'fan_speed')
+
+temperature['low'] = fuzz.trimf(temperature.universe, [0, 0, 50])
+temperature['medium'] = fuzz.trimf(temperature.universe, [0, 50, 100])
+temperature['high'] = fuzz.trimf(temperature.universe, [50, 100, 100])
+
+fan_speed['low'] = fuzz.trimf(fan_speed.universe, [0, 0, 50])
+fan_speed['medium'] = fuzz.trimf(fan_speed.universe, [0, 50, 100])
+fan_speed['high'] = fuzz.trimf(fan_speed.universe, [50, 100, 100])
+
+rule1 = ctrl.Rule(temperature['low'], fan_speed['low'])
+rule2 = ctrl.Rule(temperature['medium'], fan_speed['medium'])
+rule3 = ctrl.Rule(temperature['high'], fan_speed['high'])
+
+fan_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
+sim = ctrl.ControlSystemSimulation(fan_ctrl)
+
+sim.input['temperature'] = 75
+sim.compute()
+
+print("Fan Speed:", sim.output['fan_speed'])
